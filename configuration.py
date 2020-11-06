2470f6ed90b53d6cd65e217651bed6555f6ab80a
roypyp <roypyp@gmail.com>
2020-11-06 16:16:33 +0200

    sdaasd

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 class ConfigClass:
     def __init__(self):
-        self.corpusPath = ''
+        self.corpusPath = 'E:\תואר\אחזור מידע\Data\date=07-08-2020'
         self.savedFileMainFolder = ''
         self.saveFilesWithStem = self.savedFileMainFolder + "/WithStem"
         self.saveFilesWithoutStem = self.savedFileMainFolder + "/WithoutStem"
--- search_engine.py
+++ search_engine.py
@@ -18,7 +18,7 @@ def run_engine():
     p = Parse()
     indexer = Indexer(config)
 
-    documents_list = r.read_file(file_name='sample3.parquet')
+    documents_list = r.read_file(file_name='covid19_07-08.snappy.parquet')
     # Iterate over every document in the file
     for idx, document in enumerate(documents_list):
         # parse the document
